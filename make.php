3fa721b05b876e6ebaf0e2abab26bae500034bef
0Neiron0 <125101458+0Neiron0@users.noreply.github.com>
2023-03-13 23:47:51 +0000

    new design

--- make.php
+++ make.php
@@ -2,12 +2,12 @@
 <html>
 <body>
 <?php
-echo "start";
+echo "start</br>";
 function replace_string_in_file($filename, $string_to_replace, $replace_with){
     $content=file_get_contents($filename);
     $content_chunks=explode($string_to_replace, $content);
     $content=implode($replace_with, $content_chunks);
-    file_put_contents("out.html", $content);
+    file_put_contents("index.php", $content);
 }
 
 
@@ -15,7 +15,11 @@ $filename="words.html";
 $string_to_replace='<<<insert_dict>>>';
 $replace_with=file_get_contents("dict.db");;
 replace_string_in_file($filename, $string_to_replace, $replace_with);
+echo 'all good</br><a href="./index.php">Go!</a>';
 
 ?>
+<script>
+window.location.replace("index.php");
+</script>
 </body>
 </html> 
